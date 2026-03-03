225fc963516a5b052aedeb9e0a6cc5cc74ff2ed4
Jossbs <benavidesjoseline2@gmail.com>
2026-03-03 08:15:14 -0600

    Commit inicial, del login de usuarios, dashboard index y módulo de alumnos avanzado.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
+
+Route::middleware('auth')->group(function (): void {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('students', StudentController::class)->only(['index', 'create', 'store']);
+});
